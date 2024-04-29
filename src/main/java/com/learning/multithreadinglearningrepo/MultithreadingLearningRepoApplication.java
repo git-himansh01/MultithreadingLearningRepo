a2b887f2b262758929a3c0be879f4132313a56bb
git-himansh01 <waytohimanshu@gmail.com>
2024-04-30 00:38:53 +0530

    Set Priority and name to the main thread

--- src/main/java/com/learning/multithreadinglearningrepo/MultithreadingLearningRepoApplication.java
+++ src/main/java/com/learning/multithreadinglearningrepo/MultithreadingLearningRepoApplication.java
@@ -6,16 +6,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MultithreadingLearningRepoApplication {
 
-    public static void main(String[] args) {SpringApplication.run(MultithreadingLearningRepoApplication.class, args);
+    public static void main(String[] args) throws InterruptedException {SpringApplication.run(MultithreadingLearningRepoApplication.class, args);
         Thread thread1 = new Thread(new Runnable() {
             @Override
             public void run() {
                 System.out.println("We are now in thread "+ Thread.currentThread().getName());
+                System.out.println("Current thread priority is "+ Thread.currentThread().getPriority());
             }
         });
+        thread1.setName("My-Main-Thread");
+        thread1.setPriority(Thread.MAX_PRIORITY);
                 System.out.println("We are in thread " + Thread.currentThread().getName()+ " before starting a new thread");
                 thread1.start();
                 System.out.println("We are in thread " + Thread.currentThread().getName()+ " after starting a new thread");
-
+                Thread.sleep(10000);
 }
 }
